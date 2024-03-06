1aa2e12191935f40b161383fb65e48673b2b235f
alexpr42 <8988@holbertonstudents.com>
2024-03-05 22:52:29 -0400

    another one

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
- * main - Entry point
+ * main - prints the name of the program followed by a new line
+ * @argc: number of arguments
+ * @argv: pointer to array of input arguments
  *
- * @argc: Counts the number of parameters that go into main
- * @argv: Pointer of array of pointers containing strings entering main
- * Return: Always 0 (Success)
+ * Return: 0 on success
  */
-int main(int argc, char **argv)
+
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
