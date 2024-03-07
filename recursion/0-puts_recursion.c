6dc1382215c366073139dbfe07fa27b4912234d0
alexpr42 <8988@holbertonstudents.com>
2024-03-07 10:24:24 -0400

    all

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,10 +3,8 @@
 /**
  * _puts_recursion - Write a function that prints a string, followed by a new
  * line.
- *
  * @s: This is the input string
  */
-
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
