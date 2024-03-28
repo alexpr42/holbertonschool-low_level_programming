b84670540fb4ea3affa04582a9cd09031a806eaa
alexpr42 <8988@holbertonstudents.com>
2024-03-27 22:52:28 -0400

    fix to few files

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -5,17 +5,16 @@
  * print_dlistint - Prints all the elements of a dlistint_t list.
  * @h: Pointer to the head of the list.
  *
- * Return: The number of nodes in the list.
+ * Return: The number of nodes.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
-	const dlistint_t *current = h;
 
-	while (current != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		count++;
 	}
 
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,16 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h>
+#include <stdlib.h>
 
 /**
- * struct dlistint_s - doubly linked list node
- * @n: integer stored in the node
- * @prev: pointer to the previous element of the list
- * @next: pointer to the next element of the list
- *
- * Description: doubly linked list node structure
- * 
+ * struct dlistint_s - Doubly linked list node structure
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct dlistint_s
 {
@@ -19,6 +16,8 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
+/* Function prototypes */
 size_t print_dlistint(const dlistint_t *h);
 
 #endif /* LISTS_H */
+
