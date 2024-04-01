019d56a1a6451283dcd2f657b47f95002e1c3e3c
alexpr42 <8988@holbertonstudents.com>
2024-04-01 10:21:32 -0400

    fix for 0-read file

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints it to the POSIX.
  * @filename: Name of the file to read.
  * @letters: Number of letters it should read and print.
  * Return: The actual number of letters it could read and print.
@@ -13,15 +13,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-
 	file_descriptor = open(filename, O_RDONLY);
 	if (file_descriptor == -1)
 		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
-
 	bytes_read = read(file_descriptor, buffer, letters);
 	if (bytes_read == -1)
 	{
@@ -29,7 +26,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(file_descriptor);
 		return (0);
 	}
-
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
@@ -37,7 +33,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(file_descriptor);
 		return (0);
 	}
-
 	free(buffer);
 	close(file_descriptor);
 
